fc5ff5ac742ff758631baac4fcd7eea5d5093bc2
JaminLee <geminihaha@seeroo.co.kr>
2024-03-07 13:19:04 +0900

    html5 draw study modify

--- test/app.js
+++ test/app.js
@@ -14,5 +14,34 @@ var dino = {
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 }
-
+dino.x += 1
 dino.draw();
+
+class Cactus {
+    constructor(){
+    this.x = 500;
+    this.y = 200;
+    this.width = 50;
+    this.height = 50;
+    }
+    draw()
+    {
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+var cactus = new Cactus();
+
+
+function callFrames(){
+    requestAnimationFrame(callFrames);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    cactus.x -= 1
+    cactus.draw();
+    dino.draw();
+}
+
+callFrames();
+
