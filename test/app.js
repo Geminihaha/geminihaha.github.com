dad77908f2ae646c97af2fea38b7530180f351c5
JaminLee <geminihaha@seeroo.co.kr>
2024-03-25 15:31:06 +0900

    game modify

--- test/app.js
+++ test/app.js
@@ -2,12 +2,12 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 canvas.width = 500;
-canvas.height = 500;
+canvas.height = 200;
 
 var dino = {
-    x : 10,
-    y : 200,
-    width : 50,
+    x : 100,
+    y : 100,
+    width : 10,
     height : 50,
     draw(){
         ctx.fillStyle = 'green'
@@ -18,11 +18,11 @@ var dino = {
 class Cactus {
     constructor(){
     this.x = 500;
-    this.y = 200;
-    this.width = 50;
+    this.y = 100;
+    this.width = 10;
     this.height = 50;
     }
-    draw()
+   draw()
     {
         ctx.fillStyle = 'red'
         ctx.fillRect(this.x, this.y, this.width, this.height)
@@ -32,9 +32,11 @@ class Cactus {
 
 var cactusArr = [];
 var timer = 0.0;
+var isJump = false;
+var animId = 0;
 
 function callFrames(){
-    requestAnimationFrame(callFrames);
+    animId = requestAnimationFrame(callFrames);
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     timer++
 
@@ -46,14 +48,51 @@ function callFrames(){
     dino.draw();
     var i = 0;
     for (i = 0 ; i < cactusArr.length; i++){
-        cactusArr[i].x--;
+        cactusArr[i].x -= 2;
         cactusArr[i].draw();
-    }
-//    cactusArr.forEach((cactusItem)=>{
-//        cactusItem.x--;
-//        cactusItem.draw();
-//    })
+        if (cactusArr[i].x == 0.0){
+            cactusArr.splice(0, 1)
+        }
+        if (dino.x >= cactusArr[i].x && dino.x <= cactusArr[i].x + 10
+            && dino.y >= cactusArr[i].y && dino.y <= cactusArr[i].y + 50 
+            )
+            {
+                console.error("crash!!");
+                cancelAnimationFrame(animId);
+                break;
+            }
 
+    }
+
+    if (isJump && dino.y >= -0.01){
+        dino.y -= 3
+        if (dino.y <= -0.01){
+            isJump = false
+        }
+    } else {
+        if (dino.y < 100){
+            dino.y += 3
+        }
+    }
+}
+
+function eventJump(){
+    isJump = true
+}
+
+function restartCallFrame(){
+    cactusArr.splice(0, cactusArr.length)
+    callFrames();
 }
 
 callFrames();
+
+//    cactusArr.forEach((cactusItem)=>{
+//        cactusItem.x--;
+//        cactusItem.draw();
+
+//         if (cactusItem.x == 0.0){
+//             cactusArr.splice
+//         }
+//    })
+
