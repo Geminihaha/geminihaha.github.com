d1e0ddcb8905dd84897523da4bb6351445546d00
JaminLee <geminihaha@seeroo.co.kr>
2024-04-17 17:54:38 +0900

    test 쪽 소스 업데이트

--- test/app.js
+++ test/app.js
@@ -4,6 +4,7 @@ var ctx = canvas.getContext('2d');
 canvas.width = 500;
 canvas.height = 200;
 var cactusArr = [];
+var cloudArr = [];
 var timer = 0.0;
 var isJump = false;
 var animId = 0;
@@ -25,15 +26,25 @@ var dino = {
     }
 }
 
+var count = {
+    x : 50,
+    y : 10,
+    width : 50,
+    height : 10,
+    draw(){
+        ctx.fillStyle = 'green'
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
 class Cactus {
     constructor(){
-    this.x = 500;
-    this.y = 100;
-    this.width = 10;
-    this.height = 50;
+        this.x = 500;
+        this.y = 100;
+        this.width = 10;
+        this.height = 50;
     }
-    draw()
-    {
+    draw(){
         // ctx.fillStyle = 'red'
         // ctx.fillRect(this.x, this.y, this.width, this.height)
         const Img = new Image(this.width, this.height);
@@ -42,7 +53,20 @@ class Cactus {
             // ctx.drawImage(Img, this.x, this.y);
         //   });
         ctx.drawImage(Img, this.x, this.y);
-        
+    }
+}
+
+class Cloud {
+    constructor(){
+        this.x = 500;
+        this.y = 10;
+        this.width = 10;
+        this.height = 50;
+    }
+    draw(){
+        const Img = new Image(this.width, this.height);
+        Img.src = "cloud.png";
+        ctx.drawImage(Img, this.x, this.y);
     }
 }
 
@@ -50,8 +74,18 @@ function callFrames(){
     animId = requestAnimationFrame(callFrames);
     timer++
 
-    // if (timer % 240 === 0){
-    if (timer % 120 === 0){
+    if (timer === 1 ||timer % 480 === 0){
+        var cloud = new Cloud();
+        cloudArr.push(cloud)
+
+        var t = 0;
+        for (t= 0; t < 2 ; t++){
+            var cloud = new Cloud();
+            cloudArr.push(cloud)
+            cloudArr[t].x = -100 + (t * 240);
+        }
+    }
+    if (timer % 80 === 0){
         var cactus = new Cactus();
         cactusArr.push(cactus)
     }
@@ -59,8 +93,20 @@ function callFrames(){
     // if (timer % 4 === 0){
     if (timer % 1 === 0){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        dino.draw();
+        count.draw();
         var i = 0;
+
+        for (i = 0 ; i < cloudArr.length; i++){
+            cloudArr[i].x -= 0.5;
+            
+            cloudArr[i].draw();
+            if (cloudArr[i].x == -2000.0){
+                cloudArr.splice(0, 1)
+            }
+        }
+
+        dino.draw();
+
         for (i = 0 ; i < cactusArr.length; i++){
             cactusArr[i].x -= 2;
             cactusArr[i].draw();
@@ -72,6 +118,7 @@ function callFrames(){
                 )
                 {
                     console.error("crash!!");
+                    alert("crash!!")
                     cancelAnimationFrame(animId);
                     break;
                 }
